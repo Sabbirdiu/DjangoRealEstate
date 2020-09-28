20f1ac3483cc48632357986a948ae55361b00afb
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-29 01:56:15 +0600

    Added news app model and admin in the project

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Post
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','author')
+    prepopulated_fields = {'slug': ('title',)}
+admin.site.register(Author)
+admin.site.register(Post,ArticleAdmin)
--- news/models.py
+++ news/models.py
@@ -10,3 +10,20 @@ class Author(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    overview = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    featured = models.BooleanField()
+    def __str__(self):
+        return self.title
+
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={
+            'slug': self.slug
+        })        
