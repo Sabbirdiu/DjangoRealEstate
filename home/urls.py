0bbc610610657880ff7f369dc745042206db48e5
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-28 23:56:44 +0600

    update the templates

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',index ),
-    path('agents/',agents ),
+    path('',index ,name='home' ),
+    path('agents/',agents,name='agents' ),
     path('agents/<int:pk>/',AgentDetailView.as_view(),name ='agent-details'),
-    path('listing/',listing ),
+    path('listing/',listing,name='listings' ),
     path('listing/<int:id>',listingdetail, name='listing' ),
 ]
