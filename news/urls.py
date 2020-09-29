e9c596515f5b0227fffce2b1e4e5bdc990d25d05
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-29 13:11:50 +0600

    Added news details

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',post,name='news')
+    path('',post,name='news'),
+    path('<slug:slug>/',newsdetails, name='post-detail' ),
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Post
 # Create your views here.
 def post(request):
@@ -7,3 +7,14 @@ def post(request):
         'post_list':post_list
     }
     return render(request,'news.html',context)
+
+def newsdetails(request,slug):
+    post_detail = get_object_or_404(Post, slug=slug)
+  
+    context = {
+        'obj': post_detail,
+        
+    }
+
+    return render(request, 'news-details.html', context) 
+
