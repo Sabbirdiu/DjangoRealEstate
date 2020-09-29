6c60c58f6f55d748261a526567ca57565f57ecaf
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-09-29 13:23:41 +0600

    Added pagination in news

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def post(request):
     post_list = Post.objects.order_by('-timestamp')
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'post_list':post_list
+        'listings':paged_listings
     }
     return render(request,'news.html',context)
 
