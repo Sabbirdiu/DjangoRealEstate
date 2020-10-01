53364f5c8ab129326e26b342086b1fcce5a45e17
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-10-01 22:01:45 +0600

    add edit profile

--- contact/views.py
+++ contact/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 # from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from home.models import Listing,Realtor
 from .models import About,Partnars,Contact
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.contrib import messages
+from .forms import UserUpdateForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def about(request):
     service_slider = Listing.objects.all().order_by('-id')[:4]
@@ -124,3 +127,48 @@ def logout(request):
       return redirect('home')        
 def myaccount(request):
     return render(request,'my-account.html')            
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        # p_form = ProfileUpdateForm(request.POST,
+        #                            request.FILES,
+        #                            instance=request.user.profile)
+        if u_form.is_valid().is_valid():
+            u_form.save()
+           
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        # p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        # 'p_form': p_form
+    }
+    return render(request,'my-account.html',context)
+
+
+def edit_profile(request):
+    # context = {}
+    # check = register_table.objects.filter(user__id=request.user.id)
+    # if len(check)>0:
+    #     data = register_table.objects.get(user__id=request.user.id)
+    #     context["data"]=data    
+      if request.method=="POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+
+        user = User.objects.get(id=request.user.id)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.username = username
+        user.email = email
+        user.save()
+
+        context["status"] = "Changes Saved Successfully"
+      return render(request,"my-account.html",context)
