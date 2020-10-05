28dde09a13c8c0a3256db8515be1123274eadd81
sabbirdiu <sabbir15-8767@diu.edu.bd>
2020-10-05 08:33:03 +0600

    added home listing details api

--- home/api/serializers.py
+++ home/api/serializers.py
@@ -4,4 +4,10 @@ from ..models import Listing
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = ('title', 'city', 'state', 'price', 'servicetype', 'rent_agreement', 'bedrooms', 'bathrooms', 'sqft', 'photo_main', 'slug','kitchen','video')
+        fields = ('title','slug', 'city', 'state', 'price', 'servicetype', 'rent_agreement', 'bedrooms', 'bathrooms', 'sqft', 'photo_main','kitchen','video')
+
+class listingDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
+        lookup_field = 'slug'        
--- home/api/urls.py
+++ home/api/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('', ListingsView.as_view()),
     # path('search', SearchView.as_view()),
-    # path('<slug>', ListingView.as_view()),
+    path('<slug>/', ListingView.as_view()),
 ]
